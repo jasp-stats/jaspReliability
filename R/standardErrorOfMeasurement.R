2eaa3ee5e437f95c75835d2ada68ef9cb5155c0e
Julius Pfadt <julius.pfadt@gmail.com>
2026-01-29 17:44:34 +0100

    better error handling for Fleiss kappa

--- R/standardErrorOfMeasurement.R
+++ R/standardErrorOfMeasurement.R
@@ -18,9 +18,6 @@
 #' @export
 standardErrorOfMeasurement <- function(jaspResults, dataset, options) {
 
-  sink(file="~/Downloads/log.txt")
-  on.exit(sink(NULL))
-
   ready <- length(options[["variables"]]) > 1
 
   dataset <- .semHandleData(dataset, options, ready)
